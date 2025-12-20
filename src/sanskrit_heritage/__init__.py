790b3f8300398c29e77e650d00c5e1065e35b18b
Sriram Krishnan <sriramk8@gmail.com>
2025-12-20 16:57:38 +0000

    Bump version to 0.1.1

--- src/sanskrit_heritage/__init__.py
+++ src/sanskrit_heritage/__init__.py
@@ -2,7 +2,7 @@ from .segmenter.interface import HeritageSegmenter
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("sanskrit_heritage")
+    __version__ = version("sanskrit-heritage")
 except PackageNotFoundError:
     # Package is not installed (e.g., local dev without -e)
     __version__ = "unknown"
